85715d547ebf41436cdae4dd86781d6a52994f24
yuu_sky <yuusky20th@gmail.com>
2022-10-26 21:26:11 +0900

    fix bug

--- src/game/gabugabu.ts
+++ src/game/gabugabu.ts
@@ -2,7 +2,7 @@ const pt1: number[] = [0, 1, 3, 2, 3, 1, 0, 2]
 const pt2: number[] = [0, 1, 0, 2, 3, 1, 3, 2]
 
 function selectPt(): number[] {
-  const patternNum = 3
+  const patternNum = 2
   let randomPt = Math.floor(Math.random() * patternNum)
   let pt: number[] = []
   switch (randomPt) {
